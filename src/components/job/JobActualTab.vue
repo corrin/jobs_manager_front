1e90a07c91e1c108a7f7a98553a6e2f2bad04035
Corrin Lakeland <lakeland@gmail.com>
2025-09-20 18:07:23 +1200

    Revert "fix: clarify invoice button text based on pricing methodology"

--- src/components/job/JobActualTab.vue
+++ src/components/job/JobActualTab.vue
@@ -232,7 +232,7 @@
                       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                     />
                   </svg>
-                  {{ isCreatingInvoice ? 'Creating...' : invoiceButtonText }}
+                  {{ isCreatingInvoice ? 'Creating...' : 'Create Invoice' }}
                 </button>
 
                 <p v-if="props.paid" class="text-xs text-slate-500 text-center">
@@ -391,17 +391,6 @@ const toBeInvoiced = computed(() => {
       : actualSummary.value.rev
 
   return Math.max(0, amountToInvoice - invoiceTotal.value)
-})
-
-const invoiceButtonText = computed(() => {
-  if (props.pricingMethodology === 'fixed_price') {
-    return 'Create Invoice from Quote'
-  } else if (props.pricingMethodology === 'time_and_materials') {
-    return 'Create T&M Invoice'
-  } else {
-    console.error(`Unknown pricing methodology: ${props.pricingMethodology}`)
-    return 'Report a bug'
-  }
 })
 
 // --- FORMATTING FUNCTIONS ---
